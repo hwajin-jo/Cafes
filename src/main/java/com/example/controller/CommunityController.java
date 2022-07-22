facec7ded4f427fdac40f43fa1a830d3c7fdfa9e
yeonho <yeonho1690@naver.com>
2022-07-22 11:16:04 +0900

    community 검색추가

--- src/main/java/com/example/controller/CommunityController.java
+++ src/main/java/com/example/controller/CommunityController.java
@@ -37,9 +37,12 @@ public class CommunityController {
 	
 	
 	@RequestMapping("/list")
-	public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
-		Page<Community> paging = this.communityService.getList(page);
+	public String list(Model model, 
+					   @RequestParam(value="page", defaultValue="0") int page, 
+					   @RequestParam(value="kw", defaultValue="") String kw) {
+		Page<Community> paging = this.communityService.getList(page, kw);
 		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
 		return "community/community_list";
 	}
 	
