3c5e50f38f8d37d9bdaa2133664565081dc166de
yhw1702 <dbsgusdn1@naver.com>
2022-07-22 10:09:05 +0900

    searchyhw

--- src/main/java/com/example/repository/QqRepository.java
+++ src/main/java/com/example/repository/QqRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entity.QnaQuestion;
@@ -15,5 +16,6 @@ public interface QqRepository extends JpaRepository<QnaQuestion, Integer> {
 	QnaQuestion findBySubjectAndContent(String subject, String content);
 	List<QnaQuestion> findBySubjectLike(String subject);
 	Page<QnaQuestion> findAll(Pageable page);
+	Page<QnaQuestion> findAll(Specification<QnaQuestion> spec, Pageable pageable);
 	
 }
--- src/main/java/com/example/service/QqService.java
+++ src/main/java/com/example/service/QqService.java
@@ -5,14 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.example.DataNotFoundException;
 import com.example.entity.Member;
+import com.example.entity.QnaAnswer;
 import com.example.entity.QnaQuestion;
 import com.example.repository.QqRepository;
 
@@ -23,12 +32,34 @@ import lombok.RequiredArgsConstructor;
 public class QqService {
 	
 	private final QqRepository qqRepository;
+	
+	private Specification<QnaQuestion> search(String kw){
+		return new Specification<QnaQuestion>() {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public Predicate toPredicate(Root<QnaQuestion> q, CriteriaQuery<?> query,
+										 CriteriaBuilder cb) {
+				query.distinct(true); //중복제거
+				Join<QnaQuestion, Member> u1 = q.join("author", JoinType.LEFT);
+				Join<QnaQuestion, QnaAnswer> a = q.join("answerList", JoinType.LEFT);
+				Join<QnaAnswer, Member> u2 = a.join("author", JoinType.LEFT);
+				return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),
+					   cb.like(q.get("content"), "%" + kw + "%"),
+//					   cb.like(u1.get("name"), "%" + kw + "%"),
+//					   cb.like(u2.get("name"), "%" + kw + "%"),
+					   cb.like(q.get("content"), "%" + kw + "%"));
+				
+			}
+		};
+	}
 
-	public Page<QnaQuestion> getList(int page){
+	public Page<QnaQuestion> getList(int page, String kw){
 		List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
 		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-		return this.qqRepository.findAll(pageable);
+		Specification<QnaQuestion> spec = search(kw);
+		return this.qqRepository.findAll(spec, pageable);
 	}
 	
 	public QnaQuestion getQuestion(Integer id) throws DataNotFoundException {
@@ -63,5 +94,7 @@ public class QqService {
 		question.getVoter().add(member);
 		this.qqRepository.save(question);
 	}
+	
+	
 
 }
